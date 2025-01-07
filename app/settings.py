758de442815be549e871955e78222477e82c1940
Mohammad Zareabidaki <mohammadzarea.bidaki@gmail.com>
2025-01-07 18:52:21 +0330

    fix static and media url (#38)
    new static url: serverstatic/
    new media url: servermedia/
    Location paths edited on nginx accordingly

--- app/settings.py
+++ app/settings.py
@@ -136,9 +136,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'serverstatic/'
 
-MEDIA_URL = 'media/'
+MEDIA_URL = 'servermedia/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 
