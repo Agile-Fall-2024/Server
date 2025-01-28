684b1f2ddd4b1e149e27bc6ea455cdecbe2d0584
mohammad <maghaie14@yahoo.com>
2025-01-28 16:04:22 +0330

    handle pictures in serializers

--- advertisement/serializers.py
+++ advertisement/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.utils.translation import gettext_lazy as _
 
 from advertisement.models import Advertisement, Category, Report, Picture
 from file.serializers import FileSerializer
@@ -12,7 +13,13 @@ class PictureSerializer(serializers.ModelSerializer):
 
 class AdvertisementSummarySerializer(serializers.ModelSerializer):
     favorite = serializers.SerializerMethodField(allow_null=True)
-    main_picture = FileSerializer(required=True)
+    main_picture = serializers.SerializerMethodField()
+
+    def get_main_picture(self, obj):
+        main_picture = obj.pictures.first()
+        if not main_picture:
+            return None
+        return FileSerializer(main_picture.picture, context=self.context).data
 
     def get_favorite(self, obj):
         user = self.context['request'].user
@@ -36,6 +43,11 @@ class AdvertisementSerializer(serializers.ModelSerializer):
             return False
         account = user.account
         return account.favorite_advertisement.contains(obj)
+
+    def validate_pictures(self, value):
+        if not value:
+            raise serializers.ValidationError(_('Pictures cannot be empty'))
+        return value
 
     def create(self, validated_data):
         pictures_data = validated_data.pop('pictures')
